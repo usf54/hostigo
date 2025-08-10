d5860d4037671a0729203d052033e4709676b36c
usf54 <youssefoumhdi12@gmail.com>
2025-08-10 19:54:34 +0100

    setup breeze authentication

--- database/migrations/0001_01_01_000000_create_users_table.php
+++ database/migrations/0001_01_01_000000_create_users_table.php
@@ -13,10 +13,11 @@ return new class extends Migration
     {
         Schema::create('users', function (Blueprint $table) {
             $table->id();
-            $table->string('name');
-            $table->string('email')->unique();
-            $table->timestamp('email_verified_at')->nullable();
+            $table->string('name', 100);
+            $table->string('email', 150)->unique();
             $table->string('password');
+            $table->string('phone', 20)->nullable();
+            $table->enum('role', ['host', 'guest', 'admin'])->default('guest');
             $table->rememberToken();
             $table->timestamps();
         });
